923898dbb049baf21137c8fe5bdb54e58d04701c
Karan Dhiman <136183168+kulbirdhiman@users.noreply.github.com>
2025-06-30 12:17:51 +0530

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
-// // middleware.ts
-// import { NextRequest, NextResponse } from "next/server";
+// middleware.ts
+import { NextRequest, NextResponse } from "next/server";
 
-// export function middleware(req: NextRequest) {
-//   const token = req.cookies.get("jwt");
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("jwt");
 
-//   if (!token) {
-//     return NextResponse.redirect(new URL("/sign-up", req.url));
-//   }
+  if (!token) {
+    return NextResponse.redirect(new URL("/sign-up", req.url));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ["/dashboard/:path*"], 
-// };
+export const config = {
+  matcher: ["/dashboard/:path*"], 
+};
