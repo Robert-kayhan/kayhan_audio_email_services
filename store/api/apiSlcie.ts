eaad1ecb74d370e34d76adb3860c39416dbac80e
Karan Dhiman <136183168+kulbirdhiman@users.noreply.github.com>
2025-07-27 10:27:29 +0530

    fix template

--- store/api/apiSlcie.ts
+++ store/api/apiSlcie.ts
@@ -2,7 +2,6 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 // import { BASE_URL } from "../constants";
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://staging.cravebuy.com/",
   baseUrl: process.env.NEXT_PUBLIC_ADDRESS,
   credentials: "include",
 });
